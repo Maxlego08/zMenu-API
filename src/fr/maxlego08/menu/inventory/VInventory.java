dc47d959d54e9d10139fbf10c63887357e31d0d4
Maxlego08 <maxlego08@sfr.fr>
2023-10-05 15:51:08 +0200

    :memo: Update inventories methods

--- src/fr/maxlego08/menu/inventory/VInventory.java
+++ src/fr/maxlego08/menu/inventory/VInventory.java
@@ -1,7 +1,104 @@
 package fr.maxlego08.menu.inventory;
 
+import fr.maxlego08.menu.zcore.utils.inventory.ItemButton;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Map;
 
 public abstract class VInventory implements Cloneable, InventoryHolder {
+
+    @Override
+    public Inventory getInventory() {
+        return null;
+    }
+
+    public boolean isClose() {
+        return false;
+    }
+
+    public int getId() {
+        return 0;
+    }
+
+    public ItemButton addItem(int slot, ItemStack item) {
+        return null;
+    }
+
+    /**
+     * Allows you to remove an item from the list of items
+     *
+     * @param slot
+     */
+    public void removeItem(int slot) {
+
+    }
+
+    /**
+     * Allows you to delete all items
+     */
+    public void clearItem() {
+
+    }
+
+    /**
+     * Allows you to retrieve all items
+     *
+     * @return
+     */
+    public Map<Integer, ItemButton> getItems() {
+        return null;
+    }
+
+    /**
+     * If the click in the inventory is disabled (which is the default)
+     * then it will return true
+     *
+     * @return vrai ou faux
+     */
+    public boolean isDisableClick() {
+        return false;
+    }
+
+    /**
+     * Allows to recover the player
+     *
+     * @return player
+     */
+    public Player getPlayer() {
+        return null;
+    }
+
+    /**
+     * Allows you to retrieve the page
+     *
+     * @return the page
+     */
+    public int getPage() {
+        return 0;
+    }
+
+    /**
+     * @return the args
+     */
+    public Object[] getArgs() {
+        return null;
+    }
+
+    /**
+     * @return the inventory
+     */
+    public Inventory getSpigotInventory() {
+        return null;
+    }
+
+    /**
+     * @return the guiName
+     */
+    public String getGuiName() {
+        return null;
+    }
 
 }
--- src/fr/maxlego08/menu/inventory/inventories/InventoryDefault.java
+++ src/fr/maxlego08/menu/inventory/inventories/InventoryDefault.java
@@ -1,4 +1,37 @@
 package fr.maxlego08.menu.inventory.inventories;
 
-public class InventoryDefault {
+import fr.maxlego08.menu.api.Inventory;
+import fr.maxlego08.menu.api.button.Button;
+import fr.maxlego08.menu.inventory.VInventory;
+
+import java.util.List;
+
+public class InventoryDefault extends VInventory {
+
+    public Inventory getMenuInventory() {
+        return null;
+    }
+
+    /**
+     * @return the oldInventories
+     */
+    public List<Inventory> getOldInventories() {
+        return null;
+    }
+
+    /**
+     * @return the maxPage
+     */
+    public int getMaxPage() {
+        return 0;
+    }
+
+    public List<Button> getButtons() {
+        return null;
+    }
+
+    public void buildButton(Button button) {
+
+    }
+
 }
