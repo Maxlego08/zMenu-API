4deea76307297a43adf6256862d36726339ae8b7
Maxlego08 <maxlego08@sfr.fr>
2024-01-15 15:29:20 +0100

    :memo: Fix errors

--- src/fr/maxlego08/menu/button/buttons/ZBackButton.java
+++ src/fr/maxlego08/menu/button/buttons/ZBackButton.java
@@ -3,6 +3,7 @@ package fr.maxlego08.menu.button.buttons;
 import fr.maxlego08.menu.api.Inventory;
 import fr.maxlego08.menu.api.InventoryManager;
 import fr.maxlego08.menu.api.button.buttons.BackButton;
+import fr.maxlego08.menu.api.requirement.Action;
 import fr.maxlego08.menu.button.ZButton;
 import fr.maxlego08.menu.inventory.inventories.InventoryDefault;
 import org.bukkit.entity.Player;
@@ -11,5 +12,4 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import java.util.List;
 
 public class ZBackButton extends ZButton implements BackButton {
-
 }
